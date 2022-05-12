5a716f7aa7220e5a592e93093c58f24e0eca39ba
Abdulghani92 <ab.ghani92@gmail.com>
2022-05-12 21:58:46 +0500

    initial commit

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthModalComponent } from './auth-modal/auth-modal.component';
+
+@NgModule({
+  declarations: [
+    AuthModalComponent
+  ],
+  exports: [
+    AuthModalComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserModule { }
