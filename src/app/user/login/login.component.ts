291669387ae9ce0ce4ee530122e1bc5ee27b8abd
salman-nadeem22 <jakosalman360@gmail.com>
2022-06-05 19:23:29 +0500

    added template form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  credentials = {
+    email: '',
+    password: ''
+  }
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    console.log(this.credentials)
   }
 
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,7 +4,7 @@ import {AuthModalComponent} from './auth-modal/auth-modal.component';
 import {SharedModule} from "../shared/shared.module";
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
-import {ReactiveFormsModule} from '@angular/forms'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -18,7 +18,8 @@ import {ReactiveFormsModule} from '@angular/forms'
   imports: [
     CommonModule,
     SharedModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class UserModule {
